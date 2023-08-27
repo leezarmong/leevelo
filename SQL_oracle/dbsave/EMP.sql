3503e9076ea9125ff4ec87cec51c83471e9bec00
leezarmong <leezarmong7@gmail.com>
2023-08-27 16:30:05 +0900

    oracle_trainning

--- SQL_oracle/dbsave/EMP.sql
+++ SQL_oracle/dbsave/EMP.sql
@@ -1,2 +1,18 @@
 
+
 desc emp;
+
+
+select * from emp;
+
+--emp테이블에서 FORD 사원과 같은 급여를 받고 있는 사원들의 정보를 검색하시오
+select * from emp where sal = (select sal from emp where ename = 'FORD');
+
+
+
+
+
+select *from emp;
+
+select *from emp where sal = (select sal from emp where ename = 'FORD');
+
