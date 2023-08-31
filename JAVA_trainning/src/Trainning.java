ad731c5a86878a5ff54b7e1cfb4635cf94452a01
leezarmong <leezarmong7@gmail.com>
2023-08-31 16:48:34 +0900

    opdate

--- JAVA_trainning/src/Trainning.java
+++ JAVA_trainning/src/Trainning.java
@@ -8,11 +8,20 @@ public class Trainning {
         int max = 0 ;
         int min = 0 ;
         max = min= Integer.parseInt(result[0]);
+        // 들어오는 매개변수가 String 이어서 int 로 형 변환을 해준다.
+
 
         for(int i = 0 ; i< result.length; i++){
 
             max = (Math.max(max, Integer.parseInt(result[i])));
             min = (Math.min(min, Integer.parseInt(result[i])));
+
+            /*
+            *  int n 을 result[i] 로 초기화 해줘도 좋지만
+            * Math 함수를 써서 형변환후 바로 바꿔주도록 했다.
+            *
+            * */
+
         }
 
 
