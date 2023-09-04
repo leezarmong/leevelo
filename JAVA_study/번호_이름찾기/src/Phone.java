912802609c84dd3f440036b336ac3127244113fe
leezarmong <leezarmong7@gmail.com>
2023-09-04 17:47:04 +0900

    study

--- JAVA_study/번호_이름찾기/src/Phone.java
+++ JAVA_study/번호_이름찾기/src/Phone.java
@@ -1,59 +1,53 @@
-import javax.swing.*;
 import java.util.Scanner;
 
 public class Phone {
-    private String name ;
-    private String tal ;
+    private String name;
+    private String tal;
 
     public Phone(String name , String tal){
         this.name = name;
-        this.tal = tal ;
+        this.tal = tal;
     }
 
     public String getName(){
-        return name;
+        return name ;
     }
-
     public String getTal(){
         return tal;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        Phone phone[] ;
+        Phone phone[];
 
-        System.out.print("인원수 :");
+        System.out.print("몇명 까지 만들까요? :");
         int num = scan.nextInt();
 
         phone = new Phone[num];
 
         for(int i = 0 ; i<phone.length; i++){
-            System.out.print("이름과 번호 입력 :");
+            System.out.print("이름과 번호를 입력해 주세요. :");
 
             String name = scan.next();
             String tal = scan.next();
-            phone[i] = new Phone(name,tal);
 
+            phone[i] = new Phone(name , tal);
         }
-        System.out.print("저장되었습니다..");
+        System.out.print("입력 완료 했습니다."+"\n");
+
         while (true){
-            System.out.print("검색할 이름 :");
+            System.out.print("검색할 이름");
             String name = scan.next();
             for(int i = 0 ; i<num; i++){
                 if(name.equals(phone[i].getName())){
-                    System.out.print(name+"번호는"+phone[i].getTal()+"입니다.");
-                    i--;
+                    System.out.print(phone[i].getName()+"의 번호는"+phone[i].getTal()+"입니다.");
+                    i++;
                     break;
                 }
             }
-            if(name.equals("그만"))
-                break;
-
+            if(name.equals("그만")) break;
         }
         scan.close();
-
-
-
     }
 }
