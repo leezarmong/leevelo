c8972713d440bf3ffa1df0b36ec89932f7216e94
leezarmong <leezarmong7@gmail.com>
2023-09-05 17:29:56 +0900

    re

--- JAVA_study/제일큰1,2,3 번째 수 곱하기/src/Solution.java
+++ JAVA_study/제일큰1,2,3 번째 수 곱하기/src/Solution.java
@@ -5,15 +5,18 @@ public class Solution {
     public int nums(int[] array){
 
         Arrays.sort(array);
+        // 매게변수로 들어온 배열을 오름차순으로 설정
 
         int len = array.length;
+        // 배열의 길이 를 len 으로 초기화.
 
-
-        int max1 = array[len-1];
-        int max2 = array[len-2];
-        int max3 = array[len-3];
+        int max1 = array[len-1]; // 뒤에서 첫번째 값
+        int max2 = array[len-2]; // 뒤에서 두번째 값
+        int max3 = array[len-3]; // 뒤에서 세번째 값
 
         for(int i = len-4; i>= 0; i--){
+            // 후 나머지 부분 순환.
+
             int num = array[i];
 
             if(num > max1){
