5c0a746d6f823023be85fd4afa50cac7e2f8e2b4
leezarmong <leezarmong7@gmail.com>
2023-09-19 17:41:47 +0900

    sql_login

--- SQL_oracle/dbsave/LOOK.sql
+++ SQL_oracle/dbsave/LOOK.sql
@@ -13,3 +13,11 @@ where member_id ='q' and member_name='q';
 
 update member set member_password = '1'
 where member_id ='q' and member_name='q';
+
+select count(*) from member
+where member_email='q' and member_name='q';
+
+select member_id from member
+where member_email ='q' and member_name='q';
+
+update set member_email='q'
