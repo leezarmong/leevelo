a405bef3c953a6842c353ac033a7ad628362e329
leezarmong <leezarmong7@gmail.com>
2023-09-21 17:07:33 +0900

    BookVO_DAO_SQL

---
+++ project/look/src/main/java/com/look/model/book/impl/BookDAO.java
@@ -0,0 +1,21 @@
+package com.look.model.book.impl;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.look.model.book.BookVO;
+
+@Repository
+public class BookDAO {
+
+	@Autowired
+	SqlSessionTemplate sqlsession;
+	
+	
+	//작가 등록
+	public void bookEnroll(BookVO vo) {
+		sqlsession.insert("BookDAO.bookEnroll",vo);
+	}
+	
+}
