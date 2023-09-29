03d7bae641ff42a4b9f054c970ed657b1da97088
leezarmong <leezarmong7@gmail.com>
2023-09-29 17:03:04 +0900

    sql_table

--- SQL_oracle/dbsave/LOOK.sql
+++ SQL_oracle/dbsave/LOOK.sql
@@ -9,3 +9,41 @@ select * from vam_book;
 
 select * from vam_book;
 
+
+
+create table vam_nation(
+   nationId varchar2(2) primary key,
+    nationName varchar2(50)
+);
+insert into vam_nation values ('01', '국내');
+insert into vam_nation values ('02','국외');
+
+select * from vam_nation ;
+
+
+create table vam_nation(
+   nationId varchar2(2) primary key,
+    nationName varchar2(50)
+);
+
+create table vam_author(
+    authorId number generated as identity (start with 1) primary key,
+    authorName varchar2(50),
+    nationId varchar2(2),
+    authorIntro long,
+    foreign key (nationId) references vam_nation(nationId)
+    -- vam_author 의 nationId 가 vam_nation의 pk 인 nationId 를 참조하게 된다. 
+);
+
+
+insert into vam_author(authorName, nationId, authorIntro) values('유홍준', '01', '작가 소개입니다' );
+insert into vam_author(authorName, nationId, authorIntro) values('김난도', '01', '작가 소개입니다' );
+insert into vam_author(authorName, nationId, authorIntro) values('폴크루그먼', '02', '작가 소개입니다' );
+
+
+select * from vam_nation;
+select * from vam_author;
+
+
+select * from vam_nation natural join vam_author;
+-- natural join 
