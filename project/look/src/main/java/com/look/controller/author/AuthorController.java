d373ba05fc4d05c6b79267d97e23781754e3153b
leezarmong <leezarmong7@gmail.com>
2023-10-06 14:50:50 +0900

    pop

--- project/look/src/main/java/com/look/controller/author/AuthorController.java
+++ project/look/src/main/java/com/look/controller/author/AuthorController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -66,8 +67,10 @@ public class AuthorController {
 	}
 	
 	//작가 검색 팝업창 
-	@RequestMapping(value="/authorPop")
+	@RequestMapping(value="/authorPop" , method = RequestMethod.GET)
 	public String authorPop() throws Exception{
+		
+		
 		
 		return "admin/adminpop/authorPop";
 	}
