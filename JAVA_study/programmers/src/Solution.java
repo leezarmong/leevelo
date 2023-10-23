22a653eefa7736fe82fe834e21426a25fbd8aaf7
leezarmong <leezarmong7@gmail.com>
2023-10-23 14:32:54 +0900

    LowerUpper

--- JAVA_study/programmers/src/Solution.java
+++ JAVA_study/programmers/src/Solution.java
@@ -3,10 +3,28 @@ import java.util.Scanner;
 public class Solution {
 
     public String solution(String my_string) {
-       String answer = "";
+        String answer = my_string;
+        String index = "";
+
+        for(int i = 0 ;i< answer.length(); i++){
+            char c = answer.charAt(i);
+
+            if(Character.isUpperCase(c)){
+                index += Character.toLowerCase(c);
+            }
+            else
+                index += Character.toUpperCase(c);
+        }
 
 
-        return answer.toLowerCase();
+        return index;
+    }
+
+    public static void main(String[] args) {
+        String k = "feffFFfFfF";
+
+        Solution so = new Solution();
+        System.out.print(so.solution(k));
     }
 
 
