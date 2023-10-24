24565e69fd1e716c479fddea083a864f062b791f
leezarmong <leezarmong7@gmail.com>
2023-10-24 15:32:04 +0900

    Solution6

---
+++ JAVA_study/programmers/src/Solution5.java
@@ -0,0 +1,16 @@
+public class Solution5 {
+
+    public int solution(int n) {
+        int sum = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+
+                sum += i;
+            }
+
+        }
+            return sum;
+        }
+    }
