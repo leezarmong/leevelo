91adf024c94a48d585e92312959c2d1e008e0331
leezarmong <leezarmong7@gmail.com>
2023-11-15 21:13:13 +0900

    예약시스템 final

--- JAVA_study/알고리즘/src/예약.java
+++ JAVA_study/알고리즘/src/예약.java
@@ -1,73 +1,125 @@
 import java.util.Scanner;
 
 class Seatt {
-    private String[] Seatt;
+    private String seatt[];
 
+    //생성자
     public Seatt() {
-        Seatt = new String[10];
+        seatt = new String[10];
 
-        for (int i = 0; i < Seatt.length; i++) {
-            Seatt[i] = "---";
+        for (int i = 0; i < seatt.length; i++) {
+            seatt[i] = "---";
         }
     }
 
-    public void Showw() {
-        for (int i = 0; i < Seatt.length; i++) {
-            System.out.print(Seatt[i] + " ");
+    public void check() {
+        for (int i = 0; i < seatt.length; i++) {
+            System.out.print(seatt[i] + " ");
         }
         System.out.println();
     }
 
-    public void Sett(String name, int num) {
-        Seatt[num - 1] = name;
+    public void insert(String name, int num) {
+        seatt[num - 1] = name;
     }
 
-    public boolean reSett(String name) {
-        for (int i = 0; i < Seatt.length; i++) {
-            if (name.equals(Seatt[i])) {
-                Seatt[i] = "---";
+    public boolean reInsert(String name) {
+
+        for (int i = 0; i < seatt.length; i++) {
+            if (name.equals(seatt[i])) {
+                seatt[i] = "---";
             }
             return true;
         }
         return false;
     }
+
 }
 
-class Reser{
-    Scanner scan = new Scanner(System.in);
-    private Seatt[] sea;
-    private String[] ggg = {"A","B","C"};
+class application {
+    static Scanner scan = new Scanner(System.in);
+    Seatt[] s;
+    String[] grade = {"A", "B", "C"};
 
-    public Reser(){
-        sea = new Seatt[3];
-        for(int i=0; i<sea.length; i++){
-            sea[i] = new Seatt();
+    //생성자.
+    public application() {
+        s = new Seatt[3];
+        for (int i = 0; i < s.length; i++) {
+            s[i] = new Seatt();
         }
     }
 
-    public void Res(){
-        System.out.print("좌석 S 1, A 2, B 3");
-        int gra = scan.nextInt();
-        System.out.print(ggg[gra-1]+">>");
-        sea[gra-1].Showw();
+    public void appling() {
+        System.out.print("좌석 예약 A(1) B(2) C(3)");
+        int grade = scan.nextInt();
+        System.out.print(s[grade - 1] + ">>");
+        s[grade - 1].check();
 
-        System.out.print("name ::");
+        System.out.print("예약 하실 분의 이름 : ");
         String name = scan.next();
-        System.out.print("좌석 ::");
+
+        System.out.print("예약 하실 자리 1~10 : ");
         int num = scan.nextInt();
-
-        sea[gra-1].Sett(name , num);
+        s[grade - 1].insert(name, num);
     }
 
-    public void checc(){
-        for(int i=0; i<sea.length; i++){
-            System.out.print(ggg[i]+" ");
-            sea[i].Showw();
+    // 예약 현황 .
+    public void show() {
+
+        //3개의 벼열을 돌려준다.
+        for (int i = 0; i < s.length; i++) {
+
+            //grade의 i번째 열을 보여준다.
+            System.out.print(grade[i] + ">>");
+            //for문 의 i 번째 출력.
+            s[i].check();
         }
-        System.out.println("조회 완료.. ");
     }
 
+    public void delete() {
+
+        System.out.print("좌석 A(1) B(2) C(3)");
+        int gra = scan.nextInt();
+        System.out.print(grade[gra - 1]);
+        s[gra - 1].check();
+
+        System.out.print("삭제 하실 회원 이름");
+        String name = scan.next();
+        boolean result = s[gra - 1].reInsert(name);
+
+        if (result == true) {
+            System.out.print("삭제 되었습니다.");
+        } else
+            System.out.print("회원 정보가 없습니다.");
+    }
+
+    public void run() {
+
+        while (true) {
+            System.out.print("예약1 , 현황2 , 예약취소3");
+            int result = scan.nextInt();
+            switch (result) {
+                case 1:
+                    appling();
+                    break;
+
+                case 2:
+                    show();
+                    break;
+
+                case 3:
+                    delete();
+                    break;
+                case 4:
+                    return;
+            }
+        }
+    }
 }
 
 public class 예약 {
+    public static void main(String[] args) {
+        application al = new application();
+        al.run();
+    }
 }
