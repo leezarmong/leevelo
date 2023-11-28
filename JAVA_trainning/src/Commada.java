a1f5bc690ff531b5fcfdc86dc138f83ef57ac5ad
leezarmong <leezarmong7@gmail.com>
2023-11-28 20:54:26 +0900

    coomma

--- JAVA_trainning/src/Commada.java
+++ JAVA_trainning/src/Commada.java
@@ -1,46 +1,25 @@
 public class Commada {
 
-   public String comma (String str){
-       int len = str.length();
-       //String 의 길이
-       int count =0;
-       // 3칸 을 나눌 카운트
+    public String arra(String nums) {
+        int len = nums.length();
+        int count = 0;
 
-       StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder(nums); // Using the input string directly
 
-       for(int i=len-1; i>=0; i--){
+        for (int i = len - 1; i >= 0; i--) { // Fixing the loop boundaries
 
-           sb.insert(0,str.charAt(i));
-           count++;
+            count++;
+            if (count % 3 == 0 && i > 0) {
+                sb.insert(i, ",");
+            }
+        }
 
-           if(count%3==0 && i >0){
-               sb.insert(0,",");
-           }
-       }
-
-       return sb.toString();
-   }
-
-   public String coco (int num){
-       String str = Integer.toString(num);
-       int count =0;
-
-       StringBuffer sb = new StringBuffer();
-       for(int i=str.length()-1 ; i>=0; i--){
-           sb.insert(0,str.charAt(i));
-           count++;
-           if(count%3==0 && i>0){
-               sb.insert(0,",");
-           }
-       }
-       return sb.toString();
-   }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
-        Commada cd = new Commada();
-
-        String nums ="123456";
-        int num = 12345;
-        System.out.print(cd.coco(num));
+       Commada sb = new Commada();
+       String k = "12345678";
+       System.out.print(sb.arra(k));
     }
 }
