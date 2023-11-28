bc94b5b72e2c9fb93cc94f70b7e26b347956d199
leezarmong <leezarmong7@gmail.com>
2023-11-28 18:40:53 +0900

    trainning

---
+++ JAVA_trainning/src/입력한배열수로뒤집기.java
@@ -0,0 +1,37 @@
+public class 입력한배열수로뒤집기 {
+
+   // StringBuffer 사용
+   public int num (int[] arra){
+        int result =0;
+
+        int[] b = new int[arra.length];
+
+        for(int i=0; i<arra.length; i++){
+            b[i] = arra[arra.length-1 -i];
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        for(int n : b){
+            sb.append(n);
+        }
+        result = Integer.parseInt(sb.toString());
+
+        return result;
+    }
+
+    //String 형 변환
+    public int nums2 (int[] arra2 ){
+       int result =0;
+
+       String str = "";
+
+       for(int i=arra2.length; i>=0; i--){
+           str += arra2[i];
+       }
+
+       result = Integer.parseInt(str);
+
+       return result;
+    }
+}
