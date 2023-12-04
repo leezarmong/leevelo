a5f99b3391f094c16fefca9b1779163cd30cf64b
leezarmong <leezarmong7@gmail.com>
2023-12-04 22:27:58 +0900

    hashmap

--- JAVA_trainning/src/QuizHashMap.java
+++ JAVA_trainning/src/QuizHashMap.java
@@ -1,11 +1,10 @@
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class QuizHashMap {
-    Scanner scan = new Scanner(System.in); // scanner instance
-    HashMap<String , String> v = new HashMap<>();   //hashmap instance
+    Scanner sc = new Scanner(System.in);
+    HashMap<String , String> v = new HashMap<>();
 
     public QuizHashMap(){
         v.put("멕시코", "멕시코시티");
@@ -20,21 +19,19 @@ public class QuizHashMap {
     }
 
     public void insert(){
-        System.out.println(v.size()+" 개 가 등록 되어있습니다.");
-        Set<String> keys = v.keySet();
-        System.out.println(keys);
+        System.out.println("현재"+v.size()+"개의 나라와 수도가 입력되어있습니다.");
 
         while(true){
-            System.out.println("나라 수도 입력해주세요 ex)) 대한민국 서울");
-            String country = scan.next();
+            System.out.println("나라 와 수도 ex) 한국 서울");
+            String country = sc.next();
 
             if(country.equals("그만")){
                 break;
             }
-            String capital = scan.next();
-
+            String capital = sc.next();
             if(v.containsKey(country)){
-                System.out.println("등록 된 나라 입니다.");
+                System.out.println(country+"이미 존재 합니다.");
+                continue;
             }
             v.put(country,capital);
         }
@@ -47,13 +44,13 @@ public class QuizHashMap {
 
             System.out.println(keys);
             System.out.print("삭제 하고싶은 나라 이름 입력해 주세요.");
-            String deletecountry = scan.next();
+            String deletecountry = sc.next();
 
             if(deletecountry.equals("그만")){
                 break;
             }
 
-            boolean result = false;
+            boolean result = false; // 아닐 경우 boolean.
 
             for(int i=0; i<v.size();i++){
                 if(v.get(i).equals(deletecountry)){
@@ -65,14 +62,57 @@ public class QuizHashMap {
             if(!result){
                 System.out.println("그런 나라 없습니다. ");
             }
+        }
+    }
 
+    public void play() {
+        while(true) {
+            Set<String> keys = v.keySet();  // set 에 hashmap 의 key 의 집단을 입력 시켜준다.
+            Object [] array = (keys.toArray());     // 만들어준 set 의 keys 를 하나의 배열인 객체로 선언.
+            int index = (int)(Math.random()*array.length); // 랜덤한 위치 결정
+
+            // 문제(나라)와 정답(수도)을 결정한다.
+            String question = (String)array[index];     // 랜덤값의 배열
+            String answer = v.get(question);    // 그리고 비교할 답을 아까 질문한 해당 index의 값으로 설정.
+
+            // 문제를 출력한다.
+            System.out.print(question + "의 수도는? ");
+
+            String capitalFromUser = sc.next(); // 사용자의 입력
+            if(capitalFromUser.equals("그만")) {
+                break;
+            }
+            if(capitalFromUser.equals(answer))      //hashmap은 키를 입력하면 값이 나오며 값을 비교할때 key , value 중 value 와 비교하게된다.
+                System.out.println("정답!!");
+            else
+                System.out.println("아닙니다!!");
+        }
+    }
+
+    public void run() {
+        System.out.println("***수도 맞추기 게임을 시작합니다. ***");
+        while(true) {
+            System.out.print("입력:1, 퀴즈:2, 삭제:3, 종료:4>>");
+            int select = sc.nextInt();
+            switch(select) {
+                case 1:
+                    insert(); break;
+                case 2:
+                    play(); break;
+                case 3:
+                    delete(); break;
+                case 4:
+                    sc.close();
+                    System.out.println("게임을 종료합니다.");
+                    return;
+            }
         }
     }
 
     public static void main(String[] args) {
         QuizHashMap q = new QuizHashMap();
-
-        q.insert();
+        q.run();
     }
-
 }
+
+
