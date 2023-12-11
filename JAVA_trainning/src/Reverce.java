e9d063715cfdf342f46cdc7d870e7f0aae59544f
leezarmong <leezarmong7@gmail.com>
2023-12-11 17:35:18 +0900

    study

--- JAVA_trainning/src/Reverce.java
+++ JAVA_trainning/src/Reverce.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Reverce {
 
     //문자열 뒤집기.
@@ -41,11 +44,85 @@ public class Reverce {
 
         StringBuffer answer = new StringBuffer();
 
-        for(int i=0; i<arra1.length; i++){
+        for (int i = 0; i < arra1.length; i++) {
             answer.append(arra1[i]);
         }
 
         return Integer.parseInt(answer.toString());
+    }
+
+    //입력한 수 배열로 뒤집기
+    public int[] arra1(int nums) {
+        int[] result = {};
+
+        String answer = Integer.toString(nums);
+        result = new int[answer.length()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.parseInt(answer.substring(result.length - 1 - i, result.length - i));
+
+        }
+
+        return result;
+    }
+
+    // 입력한 수를 배열로 String
+    public int[] arra2(String nums) {
+        int[] result = {};
+
+        result = new int[nums.length()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Character.getNumericValue(nums.charAt(i));
+        }
+
+        return result;
+    }
+
+    //입력한 수 배열로 int
+    public int[] arra3(int nums) {
+        int[] result = {};
+        String answer = Integer.toString(nums);
+
+        result = new int[answer.length()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Character.getNumericValue(answer.charAt(i));
+
+        }
+
+        return result;
+    }
+
+    //입력한 수 뒤집기
+    public int nums5(int nums) {
+        int result = 0;
+
+        while (nums != 0) {
+            int digit = nums % 10;
+
+            result = result * 10 + digit;
+
+            nums /= 10;
+        }
+
+        return result;
+    }
+
+    //입력한 수 오름 차순 내림차순
+    public int nums6 (int nums){
+        String[] answer = Integer.toString(nums).split("");
+        String index ="";
+
+        Arrays.sort(answer, Collections.reverseOrder());
+
+        for(String num : answer){
+            index += num;
+        }
+
+        int result = Integer.parseInt(index);
+
+        return result;
     }
 
 
