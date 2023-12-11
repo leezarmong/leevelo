fa869186bdb1b7fd93d93ee7736abb64e9d01153
leezarmong <leezarmong7@gmail.com>
2023-12-11 16:36:08 +0900

    comma

--- JAVA_trainning/src/Comma.java
+++ JAVA_trainning/src/Comma.java
@@ -2,46 +2,47 @@ import java.util.Scanner;
 
 public class Comma {
 
-    public static boolean excaption(String str){
+    public static boolean exception(String nums) { // 범위 에 벗어 나지 않도록 하기.
 
-        for(char c : str.toCharArray()){
-            if(c<'0' || c>'9'){
+        for (char c : nums.toCharArray()) {
+            if (c < '0' && c > '9') {
                 return false;
             }
         }
-        return  true;
+        return true;
     }
 
-    public static String commaing(String str){
-        int len = str.length();
-        int count =0;
+    public String comma(String nums) {
 
-        StringBuffer result =new StringBuffer();
-        for(int i = len-1; i>=0;i--){
+        int len = nums.length();
+        int count = 0;
 
-            result.insert(0,str.charAt(i));
-            count ++;
+        StringBuffer answer = new StringBuffer();
 
-            if(count %3==0 && i>0){
-                result.insert(0,",");
+        for (int i = len - 1; i >= 0; i--) {
+            answer.insert(0, nums.charAt(i));
+            count++;
+
+            if (count % 3 == 0 && i > 0) {
+                answer.insert(0, ",");
             }
         }
-        return result.toString();
+        answer.append(" 원");
+        return answer.toString();
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Comma cm = new Comma();
 
-        System.out.print("번호 입력해줏에ㅛ :");
-        String k = scan.nextLine();
+        System.out.println("금액을 입력해주세요.");
+        String nums = scan.next();
 
-        if(!excaption(k)){
-            System.out.print("번호 입력햊수에ㅛ..");
-            return ;
+        if (!exception(nums)) {
+            System.out.println("범위를 벗어 났습니다.");
+            return;
         }
+        System.out.println(cm.comma(nums));
 
-        String result = commaing(k);
-        System.out.print(result);
     }
-
 }
