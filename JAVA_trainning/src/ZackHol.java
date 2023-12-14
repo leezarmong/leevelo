dddeb5a24069120efca96a6d5a0d1805b41bc643
leezarmong <leezarmong7@gmail.com>
2023-12-14 20:21:16 +0900

    holzzac

--- JAVA_trainning/src/ZackHol.java
+++ JAVA_trainning/src/ZackHol.java
@@ -3,16 +3,40 @@ import java.util.Scanner;
 
 public class ZackHol {
 
-    public int[][] arra (int num){
+    public int[][] arra (int[] num){
+        int zzac=0;
+        int hol=0;
+
+        for(int i=0; i<num.length; i++){
+
+            if(num[i]%2==0){
+                zzac ++;
+            }
+            else{
+                hol ++;
+            }
+        }
+
+        int[] zzacArra = new int[zzac];
+        int[] holArra = new int[hol];
+
+        int zzacIndex = 0 ;
+        int holIndex = 0 ;
+
+        for(int i=0; i<num.length; i++){
+            if(num[i]%2 ==0){
+                zzacArra[zzacIndex ++] = num[i];
+            }
+            else
+                holArra[holIndex++] = num[i];
+        }
+        int[][] result = new int[2][];
+
+        result[0] = zzacArra;
+        result[1] = holArra;
 
 
-
-
-
-
-
-
-        return new int[4][];
+        return result;
     }
 
     public static void main(String[] args) {
