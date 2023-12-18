d60d76f576b6bd8d2bca050774bda9af2bf1792e
leezarmong <leezarmong7@gmail.com>
2023-12-18 22:20:37 +0900

    programmersStudy

--- JAVA_study/programmers/src/PhoneNumber.java
+++ JAVA_study/programmers/src/PhoneNumber.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhoneNumber {
 
     private static class PhoneNumber2{
@@ -11,6 +14,36 @@ public class PhoneNumber {
         public String toString(){
             return "PhoneNumber{"+"phoneNumber='"+phoneNumber+'\''+'}';
         }
+    }
+
+    private static class Person {
+        public final String name;
+        private final List<PhoneNumber2> numbers;
+
+        public Person(String name){
+            this.name = name;
+            numbers = new ArrayList<>();
+        }
+
+
+        // number 가 전화번호 형식이면 추가하고 true 반환
+        // 전화번호 형식이 아니면 추가하지 않고 false 반환
+
+       public void addPhoneNumber(PhoneNumber2 number){
+            numbers.add(number);
+       }
+
+        public boolean addPhoneNumber(String number){
+
+            for(char c : number.toCharArray()){
+                if(!Character.isDigit(c)){
+                    return false;
+                }
+            }
+            numbers.add(number);
+            return true;
+        }
+
     }
 
     public static void main(String[] args) {
