0dd7aabbd2ad4f1cf6e144a970924ae9f98332b2
leezarmong <leezarmong7@gmail.com>
2023-12-19 22:45:33 +0900

    삼각달팽이

--- JAVA_study/programmers/src/삼각달팽이.java
+++ JAVA_study/programmers/src/삼각달팽이.java
@@ -2,35 +2,52 @@ import java.util.Scanner;
 
 public class 삼각달팽이 {
 
-    public static void main(String[] args) {
-            Scanner scan = new Scanner(System.in);
-        int[][] b= new int[5][];
+    public int[] solution(int n){
+        int[][] triangle = new int[n][n];
+        int v = 1;
+        int x = 0;
+        int y = 0;
 
-        b[0]= new int[0];
-        b[1]= new int[1];
-        b[2]= new int[2];
-        b[3]= new int[3];
-        b[4]= new int[4];
-
-        int count=1;
-        for(int i=0; i<b.length; i++){
-
-           for(int j=0; j<i; j++){
-
-               b[i][j]=  count++;
-           }
-        }
-
-
-        for(int i=0; i<b.length; i++){
-
-            for(int j=0; j<b[i].length; j++){
-
-                System.out.print(b[i][j]+" ");
+        while (true) {
+            // 아래로 이동
+            while (true) {
+                triangle[y][x] = v++;
+                if (y + 1 == n || triangle[y + 1][x] != 0) break;
+                y += 1;
             }
-            System.out.println();
+            if (x + 1 == n || triangle[y][x + 1] != 0) break;
+            x += 1;
+
+            // 오른쪽으로 이동
+            while (true) {
+                triangle[y][x] = v++;
+                if (x + 1 == n || triangle[y][x + 1] != 0) break;
+                x += 1;
+            }
+            if (triangle[y - 1][x - 1] != 0) break;
+            x -= 1;
+            y -= 1;
+
+            // 왼쪽 위로 이동
+            while (true) {
+                triangle[y][x] = v++;
+                if (triangle[y - 1][x - 1] != 0) break;
+                x -= 1;
+                y -= 1;
+            }
+            if (y + 1 == n || triangle[y + 1][x] != 0) break;
+            y += 1;
         }
 
+        int[] result = new int[v - 1];
+        int index = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                result[index++] = triangle[i][j];
+            }
+        }
 
+        return result;
     }
+
 }
