fe7e333d1b22d6d3a3ebc80530d5f5f5c8b7c8fd
leezarmong <leezarmong7@gmail.com>
2023-12-29 21:20:49 +0900

    문자열 압축

---
+++ JAVA_study/programmers/src/문자열압축.java
@@ -0,0 +1,15 @@
+public class 문자열압축 {
+
+    public int solution(String s) {
+        int min = Integer.MAX_VALUE;
+        /*다른 가능한 값보다 높은 값으로 설정되는데
+         *목록의 모든 값이 Integer.MAX_VALUE 보다 작기 떄문에 항상 min 이 업데이트 됨.
+         */
+
+        for (int length = 1; length <= s.length(); length++) { // 문자열은 최소 1부터 시작.
+            // 문자열 압축 후 가장 짧은 길이 선택 .
+        }
+
+        return min;
+    }
+}
