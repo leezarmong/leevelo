565b6d5a4c16b8de49745980670f625a677d1ef5
leezarmong <leezarmong7@gmail.com>
2023-12-29 22:06:46 +0900

    문자열압축

--- JAVA_study/programmers/src/문자열압축.java
+++ JAVA_study/programmers/src/문자열압축.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class 문자열압축 {
 
     public int solution(String s) {
@@ -11,5 +14,32 @@ public class 문자열압축 {
         }
 
         return min;
+    }
+
+    //압축한 문자열의 길이 변환
+    private int compress(String source, int length) {
+
+
+        return 0;
+    }
+
+    // length 길이씩 잘라 리스트에 담아줌
+    private List<String> split(String source, int length) {
+        List<String> tokens = new ArrayList<>();
+
+        for (int startIndex = 0; startIndex < source.length(); startIndex += length) {
+            // 문자열을 startIndex부터 잘라 token 리스트에 추가.
+
+            int endIndex = startIndex + length;
+            // Index 의 끝 지점은 시작점에서 의 길이
+
+            if (endIndex > source.length())
+                endIndex = source.length();
+                // 문자열 범위 밖이라면 문자열 끝 까지 정상적으로 잘릴 수 있도록 초기
+            tokens.add(source.substring(startIndex,endIndex));
+            // list추가
+        }
+
+        return tokens;
     }
 }
