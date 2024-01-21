ada58dea3d86d3f5a5a2ef3b103420ec4d12c8b4
leezarmong <leezarmong7@gmail.com>
2024-01-21 16:51:18 +0900

    arra

--- JAVA_trainning/src/Today21.java
+++ JAVA_trainning/src/Today21.java
@@ -1,6 +1,5 @@
 public class Today21 {
 
-
     // 배열 뒤집기.
     public int[] arra(int[] nums) {
 
@@ -24,6 +23,45 @@ public class Today21 {
         }
 
         result = Integer.parseInt(k);
+
+        return result;
+    }
+
+    // 입력한 수 배열로 뒤집기
+    public int[] arra3(int nums) {
+
+        String k = Integer.toString(nums);
+        int[] result = new int[k.length()];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = Integer.parseInt(k.substring(result.length - 1 - i, result.length - i));
+        }
+
+        return result;
+    }
+
+    //입력한 수를 배열로 String 일 경우
+    public int[] arra4 (String nums){
+
+        int[] result = new int[nums.length()];
+
+        for(int i=0; i<result.length; i++){
+            result[i] = Character.getNumericValue(nums.charAt(i));
+        }
+
+        return result;
+    }
+
+    //입력한 수를 배열로 int 일 경우
+    public int[] arra5 (int nums){
+
+        String k=Integer.toString(nums);
+        int[] result = new int[k.length()];
+
+        for(int i=0; i<result.length; i++){
+            char c = k.charAt(i);
+            result[i] = Integer.parseInt(String.valueOf(c));
+        }
 
         return result;
     }
