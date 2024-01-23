de6ad937fce018ff40400d8cf8183220720e1863
leezarmong <leezarmong7@gmail.com>
2024-01-23 17:34:22 +0900

    문자위치

--- JAVA_trainning/src/Today23.java
+++ JAVA_trainning/src/Today23.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Today23 {
@@ -8,8 +9,41 @@ public class Today23 {
         if (k.isEmpty()) {
             System.out.println('0');
             // 비어있을경우 0 을 출력
-        } else System.out.println(k.split(" ").length);
+        } else {// System.out.println(k.split(" ").length);
+            String[] str = k.split(" ");
+            System.out.println(str.length);
+
+            String[] strArray = new String[str.length];
+
+            for(int i=0; i<str.length; i++){
+                strArray[i] = str[i];
+            }
+
+            System.out.print("혹시 찾으시는 단어의 위치 알고싶으신가요?");
+            String search = scan.next();
+
+            int position = -1; // Default position if the word is not found
+
+            for (int i = 0; i < strArray.length; i++) {
+                if (strArray[i].equals(search)) {
+                    position = i + 1; // Positions are usually 1-indexed
+                    break; // Stop searching once the word is found
+                }
+            }
+
+            if (position != -1) {
+                System.out.println("The word '" + search + "' is at position: " + position);
+            } else {
+                System.out.println("The word '" + search + "' was not found.");
+            }
+
+            System.out.println(Arrays.toString(strArray));
+//            for(String insertst : strArray){
+//                System.out.println(insertst);
+//            }
+        }
         // 띄어쓰기를 기준한 길이를 출력해준다.
+        // 공백을 기준으로 했으니 단어의 갯수가 된다.
 
     }
 
