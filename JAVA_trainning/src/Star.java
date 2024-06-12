e3c0249d218d0db71ee65a96940199de9ee3e0b9
leezarmong <leezarmong7@gmail.com>
2024-06-12 22:54:08 +0900

    row col

--- JAVA_trainning/src/Star.java
+++ JAVA_trainning/src/Star.java
@@ -18,5 +18,15 @@ public class Star {
         }
 
 
+        for(int i=0; i<5; i++){
+
+            for(int j=0; j<5; j++){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+
     }
 }
--- JAVA_trainning/src/Today0612.java
+++ JAVA_trainning/src/Today0612.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Today0612 {
 
     //문자열 뒤집기
@@ -54,6 +59,26 @@ public class Today0612 {
         return result;
     }
 
+    //오름 내림 차순
+    public int sol (int nums){
+        String[] k = Integer.toString(nums).split("");
+        Arrays.sort(k);
+        //오름차순
+
+        Arrays.sort(k,Collections.reverseOrder());
+        //내림차순
+        int result = 0;
+        String index = "";
+
+        for(String str : k){
+            index += str;
+        }
+
+        result = Integer.parseInt(index);
+
+        return result;
+    }
+
 
     //입력한 수 배열로 뒤집기
     public int[]arra2 (int nums){
@@ -104,14 +129,101 @@ public class Today0612 {
     }
 
 
+    //홀짝 나누기
+    public int[][] zzachol (int[] nums){
+
+
+        int zzaccount = 0 ;
+        int holcount = 0 ;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] %2 ==0){
+                zzaccount++;
+            }
+            else{
+                holcount++;
+            }
+        }
+
+        int[]zzacarra = new int[zzaccount];
+        int[]holarra = new int[holcount];
+
+        int zzacindex = 0 ;
+        int holindex = 0 ;
+
+        for(int i=0; i<nums.length; i++){
+
+            if(nums[i]%2 ==0){
+                zzacarra[zzacindex++]= nums[i];
+            }
+            else
+                holarra[holindex++]=nums[i];
+        }
+
+        int[][] result = new int[2][];
+
+        result[0]= zzacarra;
+        result[1]=holarra;
+
+        return result;
+    }
+
+    public void arra(){
+
+        Scanner scan = new Scanner(System.in);
+
+        String[][] arr = new String[6][6];
+
+        int rowNum;
+        int colNum;
+        while (true) {
+            try {
+                System.out.print("row 좌표 1~4 : ");  //행
+                rowNum = scan.nextInt();
+
+                System.out.print("col 좌표 1~4 : ");  //
+                colNum = scan.nextInt();
+
+                if (rowNum < 0 || rowNum > 4 || colNum < 0 || colNum > 4) {
+                    System.out.println("잘못된 범위 입니다.");
+                } else {
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("잘못된 입력입니다.");
+                scan.next(); // Use scan.next() to consume the invalid input
+            }
+        }
+
+        arr[rowNum][colNum] = " ";
+        System.out.println("  0 1 2 3 4");
+
+        for (int row = 0; row < arr.length; row++) {
+            System.out.print(row + " ");
+
+            for (int col = 0; col < arr[row].length; col++) {
+                if (arr[rowNum][colNum].equals(arr[row][col])) {
+                    arr[row][col] = "X";
+                } else {
+                    arr[row][col] = " ";
+                }
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
 
     public static void main(String[] args) {
 
 
         Today0612 td = new Today0612();
 
-       String k = "12345678";
+        td.arra();
 
-       System.out.println(td.comma(k));
+
+
+
     }
 }
