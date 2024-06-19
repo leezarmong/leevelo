0d93f98bd5ec880d04f1b24017f6af7d74a40bd1
leezarmong <leezarmong7@gmail.com>
2024-06-19 23:10:40 +0900

    PascalsTraiangle

--- JAVA_trainning/src/PascalsTriangle.java
+++ JAVA_trainning/src/PascalsTriangle.java
@@ -9,10 +9,27 @@ public class PascalsTriangle {
 
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j <= i; j++) {
-                if (j == 0 || j == i) {
+                if (j == 0 || j == i) { //첫번 째 행 과 마지막
+
+                    /*
+                     1                Row 0
+                    1 1              Row 1
+                   1 2 1            Row 2
+                  1 3 3 1          Row 3
+                 1 4 6 4 1        Row 4
+                    *
+                    * */
                     triangle[i][j] = 1;
                 } else {
                     triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
+
+                    /*
+                    * **삼각형[4][2]**은 다음의 합:
+                        삼각형[3][1] (3개)
+                        삼각형[3][2] (3개)
+                        따라서 삼각형[4][2] = 삼각형[3][1] + 삼각형[3][2] = 3 + 3 = 6
+                    *
+                    * */
                 }
             }
         }
