a1be463ed328d708f3441314c4a2087f3dd782f0
leezarmong <leezarmong7@gmail.com>
2024-09-09 18:02:14 +0900

    test

--- JAVA_trainning/src/Test/Today0909.java
+++ JAVA_trainning/src/Test/Today0909.java
@@ -106,4 +106,70 @@ public class Today0909 {
 
         return  answer;
     }
+
+    //배열 -> 배열 뒤집기
+    public int[] a1 (int[]arr){
+
+        int[] answer = new int[arr.length];
+
+        for(int i=0; i<answer.length; i++){
+
+            answer[i] = arr[arr.length-1-i];
+        }
+
+        return answer;
+    }
+
+
+    //배열 -> 수집뒤집
+    public int a2 (int[] arr){
+
+        String k = "";
+        for (int i=arr.length-1;i>=0;i--){
+            k += arr[i];
+        }
+        int result = Integer.parseInt(k);
+
+        return result;
+    }
+
+    //수 -> 배열뒤
+    public int[]a3 (String num){
+
+        int[] resutl = new int[num.length()];
+
+        for(int i=0; i<resutl.length; i++){
+
+            resutl[i] = Integer.parseInt(num.substring(resutl.length-1-i,resutl.length-i));
+        }
+
+        return resutl;
+    }
+
+    //수 -> 배열
+    public int[]a4 (int nums){
+        String k = String.valueOf(nums);
+        int[] answer = new int[k.length()];
+        char c ;
+
+        for(int i=0; i<answer.length; i++){
+            c = k.charAt(i);
+            answer[i] = Integer.parseInt(String.valueOf(c));
+        }
+
+        return answer;
+    }
+
+    public int[]a4 (String nums){
+        int[] answer = new int[nums.length()];
+        char c ;
+        for(int i=0; i<answer.length; i++){
+            c = nums.charAt(i);
+            answer[i] = Integer.parseInt(String.valueOf(c));
+        }
+
+        return answer;
+    }
+
+
 }
