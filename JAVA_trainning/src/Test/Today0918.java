3114376c5a4df58b0fe039bdd8bbd6397563073b
leezarmong <leezarmong7@gmail.com>
2024-09-18 18:29:10 +0900

    max min

--- JAVA_trainning/src/Test/Today0918.java
+++ JAVA_trainning/src/Test/Today0918.java
@@ -1,18 +1,19 @@
 package Test;
 
+import java.util.Arrays;
+
 public class Today0918 {
 
-    public int[] arr(int nums){
+    public int[] arr(int nums) {
 
         String t = String.valueOf(nums);
         String[] arr = t.split("");
-
-        int max , min ;
+        int max, min;
 //        int num;
 
         max = min = Integer.parseInt(arr[0]);
 
-        for(int i=0; i<arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
 //            num = Integer.parseInt(arr[i]);
 //
 //            if(min > num){
@@ -21,8 +22,7 @@ public class Today0918 {
 //            if(max < num){
 //                max = num;
 //            }
-
-            max = Math.max(max,Integer.parseInt(arr[i]));
+            max = Math.max(max, Integer.parseInt(arr[i]));
             min = Math.min(min, Integer.parseInt(arr[i]));
         }
 
@@ -35,35 +35,59 @@ public class Today0918 {
     }
 
 
-
-    public int[] arr (int[] nums){
+    public int[] arr(int[] nums) {
 
         int[] answer = new int[2];
-        int max , min, num ;
+        int max, min, num;
 
         max = min = nums[0];
 
-        for(int i=0; i<nums.length; i++){
-            num = nums[i];
-            max = Math.max(max,nums[i]);
-            min = Math.min(min,nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[i]);
 
         }
-
         answer[0] = max;
         answer[1] = min;
 
         return answer;
     }
 
-    public static void main(String[] args) {
-        Today0918 td = new Today0918();
-        int k = 123456;
-        int[] answer = td.arr(k);
 
-        for( int n : answer){
-            System.out.print(n+" ");
+    //최대가 되는 수 3개 곱하기
+    public int test(int nums) {
+        String knums = String.valueOf(nums);
+        String[] arr = knums.split("");
+
+        int length = arr.length;
+        int max1, max2, max3;
+
+        max1 = max2 = max3 = Integer.parseInt(arr[0]);
+        int num;
+
+        Arrays.sort(arr);
+
+        max1 = Integer.parseInt(arr[length - 1]);
+        max2 = Integer.parseInt(arr[length - 2]);
+        max3 = Integer.parseInt(arr[length - 3]);
+
+        for (int i = arr.length - 4; i >= 0; i--) {
+            num = Integer.parseInt(arr[i]);
+
+            if (max1 < num) {
+                max3 = max2;
+                max2 = max1;
+                max1 = num;
+            } else if (max2 < num) {
+                max3 = max2;
+                max2 = num;
+            } else if (max3 < num) {
+                max3 = num;
+            }
         }
-    }
+        int answer = max1 * max2 * max3;
 
+        return answer;
+    }
 }
