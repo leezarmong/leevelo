b7767c2b212294cc8e61f0fe4e720826c251c1b3
leezarmong <leezarmong7@gmail.com>
2024-10-13 20:03:28 +0900

    todaystudey

--- JAVA_trainning/src/Test/Today1011.java
+++ JAVA_trainning/src/Test/Today1011.java
@@ -14,6 +14,74 @@ public class Today1011 {
     }
 
 
+    //배열 거꾸로
+    public int[] arr(int[] nums){
+
+        int[] answer = new int[nums.length];
+
+        for(int i=0; i<nums.length; i++){
+            answer[i]= answer[answer.length-1-i];
+        }
+
+        return answer;
+    }
+
+    //배열 수 뒤집기
+    public int nums (int[] arr){
+
+        String k = "";
+        for(int i= arr.length-1 ; i>=0; i--){
+            k += arr[i];
+        }
+
+        int[] arr2 = new int[arr.length];
+        for(int i=0; i<arr.length; i++){
+            arr2[i] = arr[arr.length-1-i];
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int index = 0 ;
+
+        for(int i : arr2){
+            sb.append(i);
+        }
+
+        int resutl = Integer.parseInt(sb.toString());
+
+        return 0 ;
+    }
+
+    // 수 를 배열로 뒤집기
+    public int[] arr2 ( int k){
+
+        String kstr = String.valueOf(k);
+        int[] result = new int[kstr.length()];
+
+        for(int i=0; i<result.length; i++){
+            result[i] = Integer.parseInt(kstr.substring(result.length-1-i,result.length-i));
+
+        }
+
+
+        return result;
+    }
+
+    //수 를 배열 로 재정의
+    public int[]arr3(int nums){
+        String kstr = String.valueOf(nums);
+        int[] answer = new int[kstr.length()];
+
+        char c;
+
+        for(int i=0; i<answer.length; i++){
+            c =  kstr.charAt(i);
+            answer[i] = Integer.parseInt(String.valueOf(c));
+        }
+
+return answer;
+    }
+
+
     public static void main(String[] args) {
         // 파스칼 삼각형 만들어 볼까?
 
