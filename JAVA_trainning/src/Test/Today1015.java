2bc72b1ceeb1c277307ef60c024afd0af250542e
leezarmong <leezarmong7@gmail.com>
2024-10-15 18:41:40 +0900

    hash

--- JAVA_trainning/src/Test/Today1015.java
+++ JAVA_trainning/src/Test/Today1015.java
@@ -1,6 +1,7 @@
 package Test;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Today1015 {
 
@@ -25,4 +26,45 @@ public class Today1015 {
 
       return resutl;
     }
+
+
+    //hashSet 중복 제거
+    public int[] hash (int[] nums){
+
+        HashSet<Integer>list = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++){
+            list.add(nums[i]);
+        }
+
+        int[] result = new int[list.size()];
+        int index = 0 ;
+
+        for(int i : list){
+            result[index ++] = i;
+        }
+
+        return result;
+    }
+
+    // 문자열 제거
+    public String[] hasSt (String str){
+
+        String[] strk = str.split(" ");
+        HashSet<String> list = new HashSet<>();
+
+        for(String k : strk){
+            list.add(k);
+        }
+
+        String[] result = new String[list.size()];
+        int index = 0;
+        for(String k : list){
+            result[index++] = k;
+        }
+
+
+        return result;
+    }
+
 }
